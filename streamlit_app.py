18e5f4c4f87fe702ca93f514839e7dc0923fd92e
digidetoxpr-eng <digidetox.pr@gmail.com>
2026-02-08 22:35:19 +0530

    Update app description with task details
    Added a section for completed and pending tasks in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,14 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "First App - to host logistic regression App"
+    "First App - to host logistic regression App
+    
+    completed tasks 
+    python libraries
+
+    pending tasks
+    UI design 
+    model save and copy to github
+    
+    "
 )
